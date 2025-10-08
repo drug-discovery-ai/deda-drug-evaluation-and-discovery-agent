d2cac5f64e2091494afb1bb39b03a0d0893f3608
joyanta55 <joyantamishu@gmail.com>
2025-10-08 10:52:35 -0400

    Remove API key

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # ---- Environment ----
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY="sk-proj-XXXX"
 export MCP_SSE_URL=http://localhost:8080/sse
 
 # ---- Start MCP Server ----
